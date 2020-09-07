5c75a83f85ba69cbb1ad301780ed1ab98b39422f
“guoyiyuhen” <guoyiyuhen@gmail.com>
2020-09-07 17:03:54 +0800

    s

--- src/main/java/com/miiti/firstapp/web/apis/AbstractBaseController.java
+++ src/main/java/com/miiti/firstapp/web/apis/AbstractBaseController.java
@@ -6,10 +6,21 @@ import com.miiti.firstapp.domain.model.user.SimpleUser;
 import com.miiti.firstapp.utils.RequestUtils;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.util.Assert;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 
-public abstract class AbstractBaseController {
+public abstract class AbstractBaseController extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                        .maxAge(3600)
+                        .allowCredentials(true);
+    }
 
     void addTriggeredBy(UserCommand command, HttpServletRequest request) {
         Assert.notNull(request.getUserPrincipal(), "User principal must be present in the request");
--- src/main/java/com/miiti/firstapp/web/apis/SeriesApiController.java
+++ src/main/java/com/miiti/firstapp/web/apis/SeriesApiController.java
@@ -11,13 +11,15 @@ import com.miiti.firstapp.web.results.*;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
+
 @Controller
-public class SeriesApiController {
+public class SeriesApiController extends AbstractBaseController{
     private SeriesService seriesService;
     private CourseService courseService;
 
